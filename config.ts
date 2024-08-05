4123d280bee209b416061a73ccd67cad7547e674
Akansel6 <akanselozdikici@gmail.com>
2024-08-05 10:30:09 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
+  url: "https://gorillamind.com/products/hmb?variant=42940013903917",
   match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
+  selector: `//*[@id="shopify-section-template--16806935101485__main"]`,
+  maxPagesToCrawl: ,
   outputFileName: "output.json",
   maxTokens: 2000000,
 };
